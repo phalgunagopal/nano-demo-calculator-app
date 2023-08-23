3d5e632e8f2e00ecbe27bef5c594fcca1e06942d
phalguna <phalgunagopal@gmail.com>
2023-08-23 22:38:19 +0530

    first commit

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    content_type=request.headers.get('Content-Type')
+    if content_type=='application/json':
+        json=request.json
+        data1=json['first']
+        data2=json['second']
+        first=(int) (data1)
+        second=(int)(data2)
+        sum=first+second
+        return jsonify(sum)
+
+    else:
+        return 'content-type not matching'
+    
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    content_type=request.headers.get('Content-Type')
+    if content_type=='application/json':
+        json=request.json
+        data1=json['first']
+        data2=json['second']
+        first=(int) (data1)
+        second=(int)(data2)
+        diff=first-second
+        return jsonify(diff)
+
+    else:
+        return 'content-type not matching'
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    # app.run(port=8080,host='0.0.0.0')
+    app.run(debug=True,port=8080)
