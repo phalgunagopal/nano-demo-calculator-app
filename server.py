83c98ccacefa6a62f60b1b310d7c0fbb65bc0b23
phalguna <phalgunagopal@gmail.com>
2023-08-23 23:09:14 +0530

    fifth commit

--- server.py
+++ server.py
@@ -31,5 +31,5 @@ def subtract():
      
 
 if __name__ == '__main__':
-    # app.run(port=8080,host='0.0.0.0')
-    app.run(debug=True,port=8080)
+    app.run(port=8080,host='0.0.0.0')
+    # app.run(debug=True,port=8080)
