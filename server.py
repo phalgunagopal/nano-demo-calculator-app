78f3dc207e5638c0e38b65202ac6cdab3a16e130
phalguna <phalgunagopal@gmail.com>
2023-08-23 22:55:59 +0530

    third commit

--- server.py
+++ server.py
@@ -9,34 +9,22 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    content_type=request.headers.get('Content-Type')
-    if content_type=='application/json':
-        json=request.json
-        data1=json['first']
-        data2=json['second']
-        first=(int) (data1)
-        second=(int)(data2)
-        sum=first+second
-        return jsonify(sum)
+    
+    
+    data1=request.json['first']
+    data2=request.json['second']
+    return jsonify({"result: ": data1+data2 })
 
-    else:
-        return 'content-type not matching'
+   
     
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    content_type=request.headers.get('Content-Type')
-    if content_type=='application/json':
-        json=request.json
-        data1=json['first']
-        data2=json['second']
-        first=(int) (data1)
-        second=(int)(data2)
-        diff=first-second
-        return jsonify(diff)
-
-    else:
-        return 'content-type not matching'
+   
+    data1=request.json['first']
+    data2=request.json['second']
+    return jsonify({"result: ": data1-data2 })
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
